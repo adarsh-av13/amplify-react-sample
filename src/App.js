6b44355a02a55ee949dc3be4e2bcd40b4b0c02fd
adarsh-av13 <dra4474@gmail.com>
2021-12-08 11:09:16 +0530

    added sample list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>Sample List</span>
       </header>
+      <div>
+        <ul className="App-list">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+          <li>Four</li>
+          <li>Five</li>
+          <li>Six</li>
+        </ul>
+      </div>
     </div>
   );
 }
